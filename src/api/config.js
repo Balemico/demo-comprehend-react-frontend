e005a84d835cbcd91faac1e8b709ffebc64bf291
Emanuel <emanuelbaldi88@gmail.com>
2020-06-23 15:31:31 +0200

    update api endpoint env

--- src/api/config.js
+++ src/api/config.js
@@ -1,5 +1,5 @@
-
-export const UPLOAD_API = '/api/upload';
-export const LANG_API = '/api/lang';
-export const SENTIMENT_API = '/api/sentiment';
-export const ENTITIES_API = '/api/entities';
+const API_ENDPOINT = process.env.API_ENDPOINT || "http://localhost:5000";
+export const UPLOAD_API = API_ENDPOINT + '/api/upload';
+export const LANG_API = API_ENDPOINT + '/api/lang';
+export const SENTIMENT_API = API_ENDPOINT + '/api/sentiment';
+export const ENTITIES_API = API_ENDPOINT + '/api/entities';
